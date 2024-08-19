d352d1a0af62f1d388a8c9c208f1c3ad276db873
akadot <muriloholiveira@hotmail.com>
2024-08-19 20:09:04 -0300

    🚧 Structuring IBaseRepository

--- Cascalho.Infra/Interfaces/IBaseRepository.cs
+++ Cascalho.Infra/Interfaces/IBaseRepository.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Linq.Expressions;
 using Cascalho.Domain.Entities;
 
 namespace Cascalho.Infra.Interfaces;
 
 public interface IBaseRepository<T> where T : Base
 {
-    
+    Task<T> Create(T entity);
+    Task<T> Update(T entity);
+    Task<T> Remove(T entity);
+    Task<T?> Get(Expression<Func<T, bool>> expression);
+    Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> expression);
 }
